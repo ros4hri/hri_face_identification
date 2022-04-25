17bafd687264e44f7034618aac3f48aed3dd52fc
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-04-25 13:21:06 +0200

    faces are now recognised, and a face database is being created

--- nodes/identify.py
+++ nodes/identify.py
@@ -8,9 +8,23 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import numpy as np
 
+import pickle
+import pathlib
+
 from deepface import DeepFace
 from deepface.commons.functions import find_input_shape
 from tensorflow.keras.preprocessing import image
+
+FACE_DB_DIRECTORY = pathlib.Path.home() / ".config/hri_face_identification"
+
+# create the FACE_DB_DIRECTORY if it does not exist yet
+FACE_DB_DIRECTORY.mkdir(parents=True, exist_ok=True)
+
+THRESHOLD = 0.40  # for cosine distance
+
+
+def cosine_distance(a, b):
+    return 1 - (np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)))
 
 
 def preprocess_face(img, target_size=(224, 224), grayscale=False):
@@ -75,6 +89,9 @@ class FaceIdentification:
 
         self.faces = {}
 
+        self.identified_faces = {}
+        self.load_face_database(FACE_DB_DIRECTORY)
+
         rospy.loginfo("Building Facenet model...")
         self.model = DeepFace.build_model("Facenet")
         print("Model input size: %s" % str(find_input_shape(self.model)))
@@ -98,10 +115,10 @@ class FaceIdentification:
             if id not in data.ids:
                 del self.faces[id]
 
-        rospy.loginfo("Currently tracking %s faces" % len(self.faces))
+        # rospy.loginfo("Currently tracking %s faces" % len(self.faces))
 
     def on_face(self, face_id, img):
-        rospy.loginfo("Got a face image for face %s!" % face_id)
+        # rospy.loginfo("Got a face image for face %s!" % face_id)
 
         img = CvBridge().imgmsg_to_cv2(img, desired_encoding="bgr8")
 
@@ -115,15 +132,75 @@ class FaceIdentification:
         # project face onto pre-trained embedding
         embedding = self.model.predict(img)[0].tolist()
 
-        print(embedding)
+        candidates = self.find_candidates(embedding)
+
+        if not candidates:
+            rospy.loginfo("Can not recognise this face. Storing it as a new person.")
+            self.store_face(face_id, embedding)
+        else:
+            if len(candidates) == 1:
+                face_id, distance = candidates[0]
+                rospy.loginfo(
+                    "Found a match with ID %s (distance: %.2f)!" % (face_id, distance)
+                )
+            else:
+                rospy.loginfo("Found more than one possible match:")
+                for face_id, distance in candidates:
+                    rospy.loginfo("  - %s (d=%.2f)" % (face_id, distance))
+
+    def find_candidates(self, embedding):
+
+        distances = {}
+
+        for face_id, target_embedding in self.identified_faces.items():
+            dst = cosine_distance(embedding, target_embedding)
+            if dst < THRESHOLD:
+                # keep the smallest distance for that face id
+                if face_id not in distances or dst < distances[face_id]:
+                    distances[face_id] = dst
+
+        # returns candidates, sorted by increasing cosine distance
+        return sorted(distances.items(), key=lambda x: x[1])
+
+    def store_face(self, face_id, embedding):
+        self.identified_faces[face_id] = embedding
+
+    def load_face_database(self, path: pathlib.Path):
+
+        db_path = path / "faces.db"
+
+        if not db_path.exists():
+            return
+
+        with open(db_path, "rb") as f:
+            self.identified_faces = pickle.load(f)
+
+        rospy.loginfo(
+            "Loaded %s face ids from stored in %s."
+            % (len(self.identified_faces), db_path)
+        )
+
+    def save_face_database(self, path: pathlib.Path):
+
+        db_path = path / "faces.db"
+
+        with open(db_path, "wb") as f:
+            pickle.dump(self.identified_faces, f)
+
+        rospy.loginfo(
+            "%s face ids stored in %s. Delete this file to erase all known identities."
+            % (len(self.identified_faces, db_path))
+        )
 
 
 if __name__ == "__main__":
-    rospy.init_node("face_identification_deepface")
+    rospy.init_node("hri_face_identification")
 
     face_id = FaceIdentification()
 
     rospy.loginfo(
-        "hri_face_identification_deepface ready. Waiting for faces to be detected"
+        "hri_face_identification ready. Waiting for face detections on /humans/faces/tracked"
     )
     rospy.spin()
+
+    face_id.save_face_database(FACE_DB_DIRECTORY)
