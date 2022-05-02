57b2da4d3d01f7fd1533d132c1a261a57898a249
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-05-02 09:31:39 +0200

    restructure with dedicated class

---
+++ src/face_recognition.cpp
@@ -0,0 +1,128 @@
+#include "face_recognition.hpp"
+
+#include <dlib/opencv.h>
+#include <ros/ros.h>
+
+#include <cstdlib>  // for rand()
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+
+Id generate_id(const int len = 5) {
+    static const char alphanum[] = "0123456789abcdef";
+    string tmp_s;
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[::rand() % (sizeof(alphanum) - 1)];
+    }
+
+    return tmp_s;
+}
+
+FaceRecognition::FaceRecognition() {
+    ROS_INFO("Loading dlib's ANN face recognition resnet weights...");
+    // And finally we load the DNN responsible for face recognition.
+    dlib::deserialize("dlib_face_recognition_resnet_model_v1.dat") >> net;
+}
+
+void FaceRecognition::processFace(const cv::Mat& cv_face) {
+    // wraps OpenCV image into a dlib image (no data copy)
+    // ATTENTION: face->aligned() should not be modified while
+    // wrapped: we first clone the image to ensure this does not
+    // happen.
+    // cv::Mat aligned_face(face->aligned().clone());
+    cv::Mat resized_face;
+    cv::resize(cv_face, resized_face, {IMG_SIZE, IMG_SIZE}, 0, 0,
+               cv::INTER_LINEAR);
+
+    dlib::matrix<dlib::rgb_pixel> face;
+    dlib::assign_image(
+        face, dlib::cv_image<dlib::bgr_pixel>(cvIplImage(resized_face)));
+
+    // calculate the face's descriptor by projecting the image on the RESnet
+    // embedding
+    auto desc = computeFaceDescriptor(face);
+
+    // search for known faces that are close to the new face in the embedding
+    // space
+    auto candidates = findCandidates(desc);
+
+    if (candidates.empty()) {
+        Id person_id = generate_id();
+
+        ROS_INFO_STREAM("New person detected; will be identified under id <"
+                        << person_id << ">");
+
+        ROS_INFO_STREAM("Computing face descriptor...");
+        person_descriptors[person_id].push_back(
+            computeRobustFaceDescriptor(face));
+        ROS_INFO_STREAM("done!");
+    } else {
+        if (candidates.size() == 1) {
+            auto& kv = *candidates.begin();
+            // we've got a match!
+            ROS_INFO_STREAM("Found a match with person "
+                            << kv.first << " (confidence: " << kv.second);
+
+            // TODO: compute & add new face descriptor for that person if too
+            // far from the original one
+        } else {
+            ROS_INFO("Found more than one possible match:");
+
+            for (const auto& kv : candidates) {
+                ROS_INFO_STREAM("  - " << kv.first << " (c=" << kv.second
+                                       << ")");
+            }
+        }
+    }
+}
+
+Features FaceRecognition::computeFaceDescriptor(
+    const dlib::matrix<dlib::rgb_pixel>& face) {
+    return net(face);
+}
+
+Features FaceRecognition::computeRobustFaceDescriptor(
+    const dlib::matrix<dlib::rgb_pixel>& face) {
+    dlib::matrix<float, 0, 1> face_descriptor =
+        mean(mat(net(jitter_image(face))));
+
+    ROS_DEBUG_STREAM(
+        "jittered face descriptor for one face: " << trans(face_descriptor));
+
+    return face_descriptor;
+}
+
+vector<dlib::matrix<dlib::rgb_pixel>> FaceRecognition::jitter_image(
+    const dlib::matrix<dlib::rgb_pixel>& img) {
+    thread_local dlib::rand rnd;
+
+    vector<dlib::matrix<dlib::rgb_pixel>> crops;
+    for (int i = 0; i < 100; ++i) crops.push_back(dlib::jitter_image(img, rnd));
+
+    return crops;
+}
+
+map<Id, float> FaceRecognition::findCandidates(Features descriptor) {
+    map<Id, float> scores;
+
+    for (const auto& kv : person_descriptors) {
+        auto person_id = kv.first;
+        for (const auto& known_descriptor : kv.second) {
+            auto distance = length(descriptor - known_descriptor);
+
+            auto score = computeConfidence(distance);
+
+            if (distance < EUCLIDIAN_THRESHOLD) {
+                if (scores.count(person_id) == 0 or scores[person_id] < score) {
+                    // first match or new best match
+                    scores[person_id] = score;
+                }
+            }
+        }
+    }
+
+    return scores;
+}
+
---
+++ src/face_recognition.hpp
@@ -0,0 +1,115 @@
+#include <dlib/dnn.h>
+#include <dlib/pixel.h>
+
+#include <map>
+#include <opencv2/core.hpp>
+#include <vector>
+
+// if the distance between 2 facial descriptors is less than this threshold, we
+// consider that the 2 faces are indeed the same person.
+const float EUCLIDIAN_THRESHOLD = 0.6;
+
+using Features = dlib::matrix<float, 0, 1>;
+
+using Id = std::string;
+
+//////////////////////////////////////////////////////////////////////////////////
+// construct the RESnet deep network with dlib
+// copy-pasted from dlib public domain example dnn_face_recognition_ex.cpp
+
+// note that the dlib model is trained for 150x150px images. As such, the below
+// marcro *can not be changed*
+#define IMG_SIZE 150
+
+template <template <int, template <typename> class, int, typename> class block,
+          int N, template <typename> class BN, typename SUBNET>
+using residual = dlib::add_prev1<block<N, BN, 1, dlib::tag1<SUBNET>>>;
+
+template <template <int, template <typename> class, int, typename> class block,
+          int N, template <typename> class BN, typename SUBNET>
+using residual_down = dlib::add_prev2<dlib::avg_pool<
+    2, 2, 2, 2, dlib::skip1<dlib::tag2<block<N, BN, 2, dlib::tag1<SUBNET>>>>>>;
+
+template <int N, template <typename> class BN, int stride, typename SUBNET>
+using block =
+    BN<dlib::con<N, 3, 3, 1, 1,
+                 dlib::relu<BN<dlib::con<N, 3, 3, stride, stride, SUBNET>>>>>;
+
+template <int N, typename SUBNET>
+using ares = dlib::relu<residual<block, N, dlib::affine, SUBNET>>;
+template <int N, typename SUBNET>
+using ares_down = dlib::relu<residual_down<block, N, dlib::affine, SUBNET>>;
+
+template <typename SUBNET>
+using alevel0 = ares_down<256, SUBNET>;
+template <typename SUBNET>
+using alevel1 = ares<256, ares<256, ares_down<256, SUBNET>>>;
+template <typename SUBNET>
+using alevel2 = ares<128, ares<128, ares_down<128, SUBNET>>>;
+template <typename SUBNET>
+using alevel3 = ares<64, ares<64, ares<64, ares_down<64, SUBNET>>>>;
+template <typename SUBNET>
+using alevel4 = ares<32, ares<32, ares<32, SUBNET>>>;
+
+using anet_type = dlib::loss_metric<dlib::fc_no_bias<
+    128,
+    dlib::avg_pool_everything<alevel0<alevel1<alevel2<alevel3<alevel4<
+        dlib::max_pool<3, 3, 2, 2,
+                       dlib::relu<dlib::affine<dlib::con<
+                           32, 7, 7, 2, 2,
+                           dlib::input_rgb_image_sized<IMG_SIZE>>>>>>>>>>>>>;
+
+//////////////////////////////////////////////////////////////////////////////////
+
+class FaceRecognition {
+   public:
+    FaceRecognition();
+
+    void processFace(const cv::Mat& face);
+
+    Features computeFaceDescriptor(const dlib::matrix<dlib::rgb_pixel>& face);
+
+    /** compute a more robust face descriptor by first generating jittered
+     * versions of the provided face, and then computing the mean descriptor
+     * on all resulting faces.
+     *
+     * Same API as computeFaceDescriptor, but accordingly much slower.
+     */
+    Features computeRobustFaceDescriptor(
+        const dlib::matrix<dlib::rgb_pixel>& face);
+
+    /** All this function does is make 100 copies of img, all slightly
+     * jittered by being zoomed, rotated, and translated a little bit
+     * differently. They are also randomly mirrored left to right.
+     */
+    std::vector<dlib::matrix<dlib::rgb_pixel>> jitter_image(
+        const dlib::matrix<dlib::rgb_pixel>& img);
+
+    /** returns a map <person_id, score> for all the person whose face
+     * descriptor(s) match (ie, distance < EUCLIDIAN_THRESHOLD) the provided
+     * descriptor.
+     *
+     * The smaller the score, the better the match. As such, the best match
+     * can be found with:
+     *
+     * ```cpp
+     * auto scores = findCandidates(descriptor);
+     * auto id = min_element(scores.begin(), scores.end(),
+     *                      [](decltype(scores)::value_type& l,
+     *                         decltype(scores)::value_type& r)
+     *                         -> bool { return l.second < r.second;}
+     *                      )->first;
+     * ```
+     */
+    std::map<Id, float> findCandidates(Features descriptor);
+
+   private:
+    float computeConfidence(float distance) {
+        return 1 - distance / EUCLIDIAN_THRESHOLD;
+    }
+
+    anet_type net;
+    std::map<Id, std::vector<Features>> person_descriptors;
+    std::map<Id, Id> face_person_map;
+};
+
